263b2c946f74c8160310641dd2da3ed5b8850f51
kaesinol <keiplyer@gmail.com>
2026-02-16 16:04:18 +0800

    :fire::bookmark: v2.0b2 Remove unused Alpine.js focus plugin and clean up related code

--- src/alpine-plugins.d.ts
+++ src/alpine-plugins.d.ts
@@ -1,9 +1,3 @@
-declare module "@alpinejs/focus" {
-  import type { PluginCallback } from "alpinejs";
-  const plugin: PluginCallback;
-  export default plugin;
-}
-
 declare module "@alpinejs/persist" {
   import type { PluginCallback } from "alpinejs";
   const plugin: PluginCallback;
